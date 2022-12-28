b144ca295c9271c0dcc9c5160f88f4d1ee71a86d
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-12-28 15:27:36 +0200

    Adds Loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,12 +5,14 @@ import { AppThumb } from './App.Styled';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 // import { Button } from 'components/Button/Button';
 import { fetchImages } from 'components/services/api';
+import { RotatingLines } from 'react-loader-spinner';
 
 export class App extends Component {
   state = {
     query: '',
     page: 1,
     items: [],
+    loading: false,
   };
 
   loadMore = () => {
@@ -21,15 +23,21 @@ export class App extends Component {
 
   componentDidUpdate(_, prevState) {
     if (prevState.page !== this.state.page) {
-      fetchImages(this.state.query, this.state.page).then(image => {
-        this.setState({
-          items: [...prevState.items, ...image.hits],
-        });
-      });
+      this.setState({ loading: true });
+      fetchImages(this.state.query, this.state.page)
+        .then(image => {
+          this.setState({
+            items: [...prevState.items, ...image.hits],
+          });
+        })
+        .finally(() => this.setState({ loading: false }));
     } else if (prevState.query !== this.state.query) {
-      fetchImages(this.state.query, this.state.page).then(image => {
-        this.setState({ items: image.hits });
-      });
+      this.setState({ loading: true });
+      fetchImages(this.state.query, this.state.page)
+        .then(image => {
+          this.setState({ items: image.hits });
+        })
+        .finally(() => this.setState({ loading: false }));
     }
   }
 
@@ -46,6 +54,15 @@ export class App extends Component {
 
         {this.state.items.length > 11 && (
           <button onClick={this.loadMore}>LOAD MORE</button>
+        )}
+        {this.state.loading && (
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+          />
         )}
         <GlobalStyle />
       </AppThumb>
