aa872f669f354956d43475b1b6333b1a7927cc0b
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-12-30 01:35:52 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,36 +1,5 @@
 import Modal from 'react-modal';
 
-// const modalStyles = {
-//   overlay: {
-//     position: 'fixed',
-//     top: 0,
-//     left: 0,
-//     right: 0,
-//     bottom: 0,
-//     backgroundColor: 'rgba(0, 0, 0, 0.80)',
-
-//     display: 'flex',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     zIndex: '1200',
-//   },
-//   content: {
-//     position: 'absolute',
-//     top: '40px',
-//     left: '40px',
-//     right: '40px',
-//     bottom: '40px',
-//     border: '1px solid #ccc',
-//     background: '#fff',
-//     overflow: 'auto',
-//     WebkitOverflowScrolling: 'touch',
-//     borderRadius: '4px',
-//     outline: 'none',
-//     padding: '20px',
-//     backgroundColor: 'black',
-//   },
-// };
-
 const modalStyles = {
   overlay: {
     position: 'fixed',
